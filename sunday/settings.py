513305e5bf583afcde298a48e6ec2f1eafc455d8
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-14 22:22:22 +0300

    change database

--- sunday/settings.py
+++ sunday/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'sunday.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'PORT': os.environ.get('DB_PORT', '5432')
     }
 }
+
 
 
 # Password validation
